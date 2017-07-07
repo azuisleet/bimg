f13420e21f566c31e7b3c7435d77d36059ffaf16
Ryan Kistner <ryan.kistner@apollobit.com>
2017-07-07 13:04:58 -0600

    Changed SVG detection regex

--- type.go
+++ type.go
@@ -32,7 +32,7 @@ type ImageType int
 
 var (
 	htmlCommentRegex = regexp.MustCompile("(?i)<!--([\\s\\S]*?)-->")
-	svgRegex         = regexp.MustCompile(`(?i)^\s*(?:<\?xml[^>]*>\s*)?(?:<!doctype svg[^>]*>\s*)?<svg[^>]*>[^*]*<\/svg>\s*$`)
+	svgRegex         = regexp.MustCompile(`(?i)^\s*(?:<\?xml[^>]*>\s*)?(?:<!doctype svg[^>]*>\s*)?<svg[^>]*>[\s\S]*<\/svg>\s*$`)
 )
 
 // ImageTypes stores as pairs of image types supported and its alias names.
